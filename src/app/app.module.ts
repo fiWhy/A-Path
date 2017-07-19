a4d6957e89cc67b424652ccc7b167db85c6e6b84
Denis <denis.buryachkovsky@gmail.com>
2017-07-19 15:41:39 +0300

    Configuring ngui/map

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,8 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { NguiMapModule } from '@ngui/map';
+
+import { GoogleMapsConfig } from "./config/googlemaps.config";
 
 import { AppComponent } from './app.component';
 import { MapComponent } from './components/map/map.component';
@@ -10,7 +13,10 @@ import { MapComponent } from './components/map/map.component';
     MapComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    NguiMapModule.forRoot({ 
+      apiUrl: `https://maps.google.com/maps/api/js?key=${GoogleMapsConfig.API_KEY}&libraries=${GoogleMapsConfig.LIBRARIES.join(",")}` 
+    }),
   ],
   providers: [],
   bootstrap: [AppComponent]
